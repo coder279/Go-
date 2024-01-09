b0080811c74d007f2f3720f95cf0cdf71934a371
coder279 <coder279@126.com>
2024-01-09 23:45:37 +0800

    go语言深入学习readme

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# Go-
-Go专家编程实战书籍练习操作方便以后可以复习巩固
+# Go语言专家编程
+
+简短的项目描述。
+
+## Go语言高级特性学习
+
+- 列出项目的主要特性和亮点。
+
+## 截图 Screenshots
+
+放置项目的截图或演示 GIF。
+
+## 开始 Get Started
+
+这里提供如何开始使用或贡献的指南。
+
+### 安装 Installation
+
+1. 克隆项目到本地
+   ```sh
+   git clone https://github.com/your_username/your_project.git
+2. 安装项目
+   ```
+    go mod tidy
+   ```
+贡献者 Contributing
+包括提交 bug 报告、提出功能请求或直接提交代码的方式。
+
+技术栈
+Golang
--- map/example1/main.go
+++ map/example1/main.go
@@ -1,5 +1,21 @@
 package main
 
+import "unsafe"
+
+// A header for a Go map.
+type hmap struct {
+	count     int    //当前保存元素个数
+	B         uint8  //buckets数组大小
+	noverflow uint16 // approximate number of overflow buckets; see incrnoverflow for details
+	hash0     uint32 // hash seed
+
+	buckets    unsafe.Pointer // array of 2^B Buckets. may be nil if count==0.
+	oldbuckets unsafe.Pointer // previous bucket array of half the size, non-nil only when growing
+	nevacuate  uintptr        // progress counter for evacuation (buckets less than this have been evacuated)
+
+	extra *mapextra // optional fields
+}
+
 func main() {
 
 }
