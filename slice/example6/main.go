6bf9d477414a77de1c377638b37af7f593c146bb
coder279 <coder279@126.com>
2024-01-09 23:03:19 +0800

    模拟map扩容过程

--- slice/example6/main.go
+++ slice/example6/main.go
@@ -4,8 +4,9 @@ import "log"
 
 func main() {
 	simple := []int{1, 2, 3, 4, 5}
-	//切片后append会覆盖原来的数值
-	simpleA := simple[1:3]
+	//切片后append会覆盖原来的数值,封印切片如果切片增加数据后则产生新的切片
+	simpleA := simple[1:3:3]
+	log.Printf("simpleA:%p simple:%p", simpleA, simple)
 	simpleA = append(simpleA, 10)
 	log.Println(simple)
 }
